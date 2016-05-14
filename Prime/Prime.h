e85eccf2eab46eb2da427faba11bd853804b1a80
Riley Testut <riley@rileytestut.com>
2016-05-13 19:24:35 -0700

    Initial Commit

---
+++ Prime/Prime.h
@@ -0,0 +1,19 @@
+//
+//  Prime.h
+//  Prime
+//
+//  Created by Riley Testut on 5/13/16.
+//  Copyright © 2016 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Prime.
+FOUNDATION_EXPORT double PrimeVersionNumber;
+
+//! Project version string for Prime.
+FOUNDATION_EXPORT const unsigned char PrimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Prime/PublicHeader.h>
+
+
