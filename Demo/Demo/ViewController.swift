dcc8924556b65ccd2675f29f545220fe13d89ecd
Riley Testut <riley@rileytestut.com>
2016-05-13 19:32:54 -0700

    Adds Demo iOS project

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Riley Testut on 5/13/16.
+//  Copyright © 2016 Riley Testut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
